65d5525de67c5022fc2dd30158e94821d02acfa0
hwang <readercn@gmail.com>
2023-11-29 00:07:13 +0100

    hello world of react-chat-elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {MessageBox, Input}  from "react-chat-elements";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          A Web UI for Twilio SMS and Whatsapp API . 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <MessageBox
+        position='left'
+        title='Burhan'
+        type='text'
+        text="Hi there !"
+        date={new Date()}
+        id='1234'
+        focus={true}
+        replyButton={false}
+        removeButton={false}
+        forwarded={false}
+        titleColor=''
+        status='received'
+        notch={false}
+        retracted={false}
+
+        
+      />
+
+      <Input
+          className='rce-example-input'
+          placeholder='Write your message here.'
+          defaultValue=''
+          //value={inputValue}
+          //onChange={handleInputChange}
+          multiline={true}
+          maxlength={300}
+          onMaxLengthExceed={() => console.log('onMaxLengthExceed')}
+          
+          
+          maxHeight={50}
+          
+          onKeyPress={(e: any) => {
+            if (e.shiftKey && e.charCode === 13) {
+              return true
+            }
+            if (e.charCode === 13) {
+              
+            }
+          }}
+      />
     </div>
+    
   );
 }
 
